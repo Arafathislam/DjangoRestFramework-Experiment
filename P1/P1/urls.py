9885edd8b59185ffe04e91c798f3055edb997a71
Arafathislam <islamarafath315@gmail.com>
2023-04-18 12:36:18 +0600

    finish

--- P1/P1/urls.py
+++ P1/P1/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('stuinfo/',views.student_detail),
+    path('stuinfo/<int:pk>',views.student_detail),
+    path('stuinfo/',views.student_list),
 ]
--- P1/api/views.py
+++ P1/api/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render
 from .models import *
 from .serializers import StudentSerializer
 from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 # Create your views here.
-def student_detail(request):
-    stu=Student.objects.get(id=2)
+def student_detail(request,pk):
+    stu=Student.objects.get(id=pk)
     seralizer=StudentSerializer(stu)
     json_data= JSONRenderer().render(seralizer.data)
 
     return HttpResponse(json_data,content_type='application/json')
+
+
+def student_list(request):
+    stu=Student.objects.all()
+    seralizer=StudentSerializer(stu,many=True)
+    # json_data= JSONRenderer().render(seralizer.data)
+
+    # return HttpResponse(json_data,content_type='application/json')
+
+    return JsonResponse(seralizer.data,safe=False)
